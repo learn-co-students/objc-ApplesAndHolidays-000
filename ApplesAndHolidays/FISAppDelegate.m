e8555d8b46dba8eae0cf9d13780b9020755f71b0
kevinjtsai <tsai.kevin.j@gmail.com>
2015-08-21 16:04:19 -0400

    finished lab

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -22,4 +22,54 @@
  
  */
 
+
+-(NSArray *)pickApplesFromFruits:(NSArray *)fruits{
+    NSPredicate *apples = [NSPredicate predicateWithFormat:@"SELF CONTAINS[cd] 'apple'"];
+    NSArray *appleArray = [fruits filteredArrayUsingPredicate:apples];
+    return appleArray;
+};
+
+-(NSArray * )holidaysInSeason:(NSString *)season inDatabase:(NSDictionary *)database{
+    NSMutableArray *holidays = [NSMutableArray array];
+    for (NSString *key in database[season]){
+        [holidays addObject: key];
+    };
+    
+    return holidays;
+};
+
+-(NSArray *)suppliesInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary*)database{
+    NSMutableArray *suppliesForHoliday = [NSMutableArray array];
+    for (NSString *supply in database[season][holiday]){
+        [suppliesForHoliday addObject:supply];
+    };
+    return suppliesForHoliday;
+};
+
+-(BOOL)holiday:(NSString *)holiday isInSeason:(NSString *)season inDatabase:(NSDictionary *)database{
+    BOOL check = [[database[season] allKeys] containsObject:holiday];
+    return check;
+};
+
+-(BOOL)supply:(NSString *)supply isInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database{
+    NSLog(@"!!!!!!!!!!!!!!!!!!!!");
+    NSLog(@"%@", database[season][holiday]);
+    BOOL check = [database[season][holiday] containsObject:supply];
+    NSLog(@"%hhd", check);
+    return check;
+};
+
+-(NSDictionary *)addHoliday:(NSString *)holiday toSeason:(NSString *)season inDatabase:(NSDictionary *)database{
+    database[season][holiday]=[NSMutableArray array];
+    return database;
+};
+
+-(NSDictionary *)addSupply:(NSString *)supply toHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database{
+    NSMutableArray *supplies = [NSMutableArray array];
+    [supplies addObject:supply];
+    database[season][holiday]=supplies;
+    return database;
+};
+
+
 @end
